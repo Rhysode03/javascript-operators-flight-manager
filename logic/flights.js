07ba2ab6890d9efb690c028f01c1a3ab6282d178
Kyle Calder <kylebcalder03@gmail.com>
2021-09-15 23:59:25 -0600

    added export

--- logic/flights.js
+++ logic/flights.js
@@ -1,7 +1,7 @@
 function Flights() {
     function calculateNumberOfFlights(passengers, capacity, flights) {
-        flights  = passengers/capacity + 1
-        
+        flights  = passengers/capacity + 1 
     }
     return calculateNumberOfFlights();
 }
+module.export = Flights();
